e507a71a7edeed281f23b80e77cca5b6322a8acb
edelsonc <charles.edelson12@ncf.edu>
2017-07-27 13:04:53 -0700

    updated documentation

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#'Micro RNA Expression Values for the Non-Human Brain Atlas.
+#'
+#'A dataset containing the normalized expression values for a the Non-Human brain atlas.
+#'
+#'
+#'@format A data frame with 8410116 rows and 6 variables:
+#'\describe{
+#'  \item{gene}{gene measured }
+#'  \item{sample_id}{Allen Institute sample ID}
+#'  \item{brain_structure}{brain structure acronym}
+#'  \item{age}{Specimen age}
+#'  \item{area}{Brain area the sample originates from}
+#'  \item{layer}{Brain layer the sample originates from}
+#'}
+#'@source \url{"https://github.com/AllenBrainAtlas/DevRhesusLMD"}
+#'
+"exprl2"
